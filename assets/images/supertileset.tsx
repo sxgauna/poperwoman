54f165a8765b83f16a024006ab0ac0333baf6f05
Sebastián Gauna <sxgauna@gmail.com>
2025-08-22 21:25:08 -0300

    20252208

--- assets/images/supertileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="map" tilewidth="16" tileheight="16" tilecount="108" columns="18">
- <image source="supertileset.png" width="288" height="96"/>
-</tileset>
